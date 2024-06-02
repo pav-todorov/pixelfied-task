b007eda7e0e2ad69fafaaf780fbd330a27333fef
P.T <->
2024-06-02 21:58:34 +0300

    Initial Commit

---
+++ Pixelfield_Task/Pixelfield_TaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pixelfield_TaskApp.swift
+//  Pixelfield_Task
+//
+//  Created by Work on 2.06.24.
+//
+
+import SwiftUI
+
+@main
+struct Pixelfield_TaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
